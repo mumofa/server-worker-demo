11c6fc89e371e532582dd46f132d4b83fa964bfb
mumofa <410649451@qq.com>
2018-08-15 11:36:16 +0800

    no message

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
-// const CHECK_CRASH_INTERVAL = 10 * 1000; // 每 10s 检查一次
-// const CRASH_THRESHOLD = 15 * 1000; // 15s 超过15s没有心跳则认为已经 crash
-// const pages = {};
-// let timer;
+const CHECK_CRASH_INTERVAL = 10 * 1000; // 每 10s 检查一次
+const CRASH_THRESHOLD = 15 * 1000; // 15s 超过15s没有心跳则认为已经 crash
+const pages = {};
+let timer;
 
-// function checkCrash() {
-//     const now = Date.now();
-//     for (var id in pages) {
-//         let page = pages[id];
-//         if ((now - page.t) > CRASH_THRESHOLD) {
-//             // 上报 crash
-//             delete pages[id];
-//         }
-//     }
-//     if (Object.keys(pages).length == 0) {
-//         clearInterval(timer);
-//         timer = null;
-//     }
-// }
+function checkCrash() {
+    const now = Date.now();
+    for (var id in pages) {
+        let page = pages[id];
+        if ((now - page.t) > CRASH_THRESHOLD) {
+            // 上报 crash
+            delete pages[id];
+        }
+    }
+    if (Object.keys(pages).length == 0) {
+        clearInterval(timer);
+        timer = null;
+    }
+}
 
-// worker.addEventListener('message', (e) => {
-//     const data = e.data;
-//     if (data.type === 'heartbeat') {
-//         pages[data.id] = {
-//             t: Date.now()
-//         }
-//         if (!timer) {
-//             timer = setInterval(function () {
-//                 checkCrash();
-//             }, CHECK_CRASH_INTERVAL);
-//         }
-//     } else if (data.type === 'unload') {
-//         delete pages[data.id];
-//     }
-// });
+self.addEventListener('message', (e) => {
+    const data = e.data;
+    if (data.type === 'heartbeat') {
+        pages[data.id] = {
+            t: Date.now()
+        }
+        if (!timer) {
+            timer = setInterval(function () {
+                checkCrash();
+            }, CHECK_CRASH_INTERVAL);
+        }
+    } else if (data.type === 'unload') {
+        delete pages[data.id];
+    }
+});
 
 var VERSION = 'v1';
 
